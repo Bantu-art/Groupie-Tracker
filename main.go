efc832217bdfc4296dac50aa4f1611f0ee95a67d
bbantu <brianbaraza40@gmail.com.com>
2024-08-19 16:36:51 +0300

    feat: Add handlers for Relations
    - Added route handlers for the relations ("/relations/") paths.

--- main.go
+++ main.go
@@ -9,13 +9,12 @@ import (
 
 func main() {
 	fmt.Println("Server running on :9000")
-
 	http.HandleFunc("/", routes.Index)
 	http.HandleFunc("/artists", routes.Artists)
 	http.HandleFunc("/artists/", routes.Artist)
 	http.HandleFunc("/dates/", routes.Dates)
 	http.HandleFunc("/locations/", routes.Locations)
-
+	http.HandleFunc("/relation/", routes.Relation)
 
 	http.ListenAndServe(":9000", nil)
 }
