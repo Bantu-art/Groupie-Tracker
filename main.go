8821f9347a8dc95f2990144b51f9ba74e6cf1cc9
bbantu <brianbaraza40@gmail.com.com>
2024-08-19 15:21:28 +0300

    feat: Add initial server setup with route handler for Artist
    - Added route handler for the artists ("/artists/") path.

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
-	"groupie-tracker/routes"
 	"net/http"
+
+	"groupie-tracker/routes"
 )
 
 func main() {
 	fmt.Println("Server running on :9000")
 
 	http.HandleFunc("/", routes.Index)
+	http.HandleFunc("/artists/", routes.Artist)
 	http.ListenAndServe(":9000", nil)
 }
